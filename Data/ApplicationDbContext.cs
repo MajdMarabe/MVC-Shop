b462d3da9e9bdade40c8c1e3e009824a4eda9247
MajdMarabe <s12027646@stu.najah.edu>
2026-01-24 16:23:58 +0200

    online DB

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,7 +11,12 @@ namespace MA_shop.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.;Database=mvc-shop;Trusted_connection=True;TrustServerCertificate=True");
+            /// for online DB
+            optionsBuilder.UseSqlServer("Server=db38976.public.databaseasp.net; Database=db38976; User Id=db38976; Password=Nr9#-Ta6e3K+; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True; ");
+           
+//for local DB
+
+           // optionsBuilder.UseSqlServer("Server=.;Database=mvc-shop;Trusted_connection=True;TrustServerCertificate=True");
         }
         
     }
